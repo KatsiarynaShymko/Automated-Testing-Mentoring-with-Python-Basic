ee9fbbacdcfc5854b9bc2a2693c560aeefb8aeba
Katsiaryna Shymko <katsiaryna_shymko@epam.com>
2025-10-07 13:06:26 +0400

    API task

---
+++ API_tests/test_fourth_group.py
@@ -0,0 +1,44 @@
+"""Tests for https://http.cat/status/404"""
+
+import pytest
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://http.cat/status/404"
+
+
+@pytest.fixture
+def response():
+    """Fixture to get HTTP response."""
+    return requests.get(URL, timeout=10)
+
+
+def test_status_code(response):
+    """Test that status code is 200."""
+    expected_status_code = 200
+    assert (
+        response.status_code == expected_status_code
+    ), f"Actual status code {response.status_code} is not {expected_status_code}"
+
+
+def test_content_type_header(response):
+    """Test that Content-Type header is text/html."""
+    expected_content_type_header = "text/html"
+    assert (
+        response.headers["Content-Type"] == expected_content_type_header
+    ), f"Content-Type header from {URL} is not {expected_content_type_header}"
+
+
+def test_h1(response):
+    """Test that h1 contains '404 Not Found'."""
+    expected_h1 = "404 Not Found"
+    soup = BeautifulSoup(response.text, "html.parser")
+    h1 = soup.find("h1", class_="text-center my-12").text
+    assert (h1 == expected_h1), f"h1 from {URL} is not {expected_h1}"
+
+
+def test_response_time(response):
+    """Test that response time is under 600ms."""
+    elapsed_ms = response.elapsed.total_seconds() * 1000
+    expected_ms = 600
+    assert (elapsed_ms < expected_ms), f"It took more than {expected_ms} ms"
