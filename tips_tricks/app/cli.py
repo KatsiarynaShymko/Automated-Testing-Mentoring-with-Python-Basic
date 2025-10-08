8cc42cef4b4b67d7d3f1996389da8c7991139cb3
Katsiaryna Shymko <katsiaryna_shymko@epam.com>
2025-10-08 13:45:29 +0400

    Fix issues

--- tips_tricks/app/cli.py
+++ tips_tricks/app/cli.py
@@ -22,45 +22,53 @@ def main():
     )
 
     parser = argparse.ArgumentParser(description="Takes file an reads it")
-    parser.add_argument("text_file", type=str, help="Name of text file")
-    parser.add_argument("output_file", type=str, help="Name of output file")
+    parser.add_argument(
+        "--input",
+        required=True,
+        type=str,
+        help="Path to the source text file")
+    parser.add_argument(
+        "--output",
+        required=True,
+        type=str,
+        help="Path to the destination file")
 
     args = parser.parse_args()
-    SOURCE_FILE_NAME = args.text_file
-    DESTINATION_FILE_NAME = args.output_file
+    SOURCE_FILE_NAME = args.input
+    DESTINATION_FILE_NAME = args.output
 
     logger = logging.getLogger("main")
 
-    if os.path.exists(SOURCE_FILE_NAME):
-        if SOURCE_FILE_NAME.endswith(".txt"):
-            try:
-                with open(SOURCE_FILE_NAME, "r", encoding="utf-8") as f, open(
-                    DESTINATION_FILE_NAME, "w", encoding="utf-8"
-                ) as d:
-                    content = f.read()
-                    symbol_count = len(content)
-                    d.write(f"Text has {symbol_count} characters")
-                    print(f"Success. Please check {DESTINATION_FILE_NAME}")
-                    logger.info(
-                        "Data is successfully written to %s", DESTINATION_FILE_NAME
-                    )
-                    return "Success"
-            except FileNotFoundError:
-                print("File is not found")
-                logger.error("File is not found: %s", SOURCE_FILE_NAME)
-                return "File is not found"
-            except Exception as e:
-                print("An unexpected error occurred:", str(e))
-                logger.exception("Unexpected error")
-                return f"An unexpected error occurred: {e}"
-        else:
-            print("Only text files are allowed")
-            logger.error("Only text files are allowed: %s", SOURCE_FILE_NAME)
-            return "Only text files are allowed"
-    else:
+    if not os.path.exists(SOURCE_FILE_NAME):
         print("File does not exist")
         logger.error("File does not exist: %s", SOURCE_FILE_NAME)
         return "File does not exist"
+
+    if not SOURCE_FILE_NAME.endswith(".txt"):
+        print("Only text files are allowed")
+        logger.error("Only text files are allowed: %s", SOURCE_FILE_NAME)
+        return "Only text files are allowed"
+
+    try:
+        with open(SOURCE_FILE_NAME, "r", encoding="utf-8") as f, open(
+                DESTINATION_FILE_NAME, "w", encoding="utf-8"
+        ) as d:
+            content = f.read()
+            symbol_count = len(content)
+            d.write(f"Text has {symbol_count} characters")
+            print(f"Success. Please check {DESTINATION_FILE_NAME}")
+            logger.info(
+                "Data is successfully written to %s", DESTINATION_FILE_NAME
+            )
+            return "Success"
+    except FileNotFoundError:
+        print("File is not found")
+        logger.error("File is not found: %s", SOURCE_FILE_NAME)
+        return "File is not found"
+    except Exception as e:
+        print("An unexpected error occurred:", str(e))
+        logger.exception("Unexpected error")
+        return f"An unexpected error occurred: {e}"
 
 
 if __name__ == "__main__":
